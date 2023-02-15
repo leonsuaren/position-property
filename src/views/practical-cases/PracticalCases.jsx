10f78ec6d8b38d4295ada2cc304e7159f72ccdb5
Leon Suarez <leonsua@gmail.com>
2023-02-15 14:19:13 -0500

    working on responsive

--- src/views/practical-cases/PracticalCases.jsx
+++ src/views/practical-cases/PracticalCases.jsx
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react';
+import { AiOutlineEdit } from 'react-icons/ai'
+import { BsQuestionLg } from 'react-icons/bs'
 import './styles.css';
 
 import { DisplayWindow } from '../../components/display-window'
@@ -7,22 +9,32 @@ export const PracticalCases = () => {
   const [fixedMenu, setFixedMenu] = useState(true);
   const [relativeButton, setRelativeButton] = useState(true);
   const [stickyTricks, setStickyTricks] = useState(true);
+  const [absoluteFeedback, setAbsoluteFeedback] = useState(true);
 
   const handleOnFixedElement = () => {
     setFixedMenu(s => !s);
     setRelativeButton(true);
     setStickyTricks(true);
+    setAbsoluteFeedback(true);
   }
 
   const handleOnShowRelativeElement = () => {
     setRelativeButton(s => !s);
     setFixedMenu(true);
     setStickyTricks(true);
-
+    setAbsoluteFeedback(true);
   }
 
   const handleOnCreateStickyEffect = () => {
     setStickyTricks(s => !s);
+    setFixedMenu(true);
+    setRelativeButton(true);
+    setAbsoluteFeedback(true);
+  }
+
+  const handleOnPositioningAbsoluteElements = () => {
+    setAbsoluteFeedback(s => !s);
+    setStickyTricks(true);
     setFixedMenu(true);
     setRelativeButton(true);
   }
@@ -31,6 +43,7 @@ export const PracticalCases = () => {
     setFixedMenu(true);
     setRelativeButton(true);
     setStickyTricks(true);
+    setAbsoluteFeedback(true);
   }, []);
 
   return (
@@ -45,6 +58,9 @@ export const PracticalCases = () => {
 
         <input type="radio" className="btn-check" name="btnradio" id="btnradio3" autoComplete="off" />
         <label className="btn btn-outline-primary" htmlFor="btnradio3" onClick={handleOnCreateStickyEffect}>Sticky</label>
+
+        <input type="radio" className="btn-check" name="btnradio" id="btnradio4" autoComplete="off" />
+        <label className="btn btn-outline-primary" htmlFor="btnradio4" onClick={handleOnPositioningAbsoluteElements}>Absolute</label>
       </div>
       <DisplayWindow headerTitle='FaceNote'>
         <div className='fecenote-content'>
@@ -75,9 +91,9 @@ export const PracticalCases = () => {
                     <p className='avatar-info-paragraph blur'>Full Stack Web Developer</p>
                   </div>
                 </div> :
-
                 <div className='avatar'>
                   <div className='avatar-picture-relative'>
+                    <div className='edit-avatar-button'><AiOutlineEdit className='edit-icon' /></div>
                     <img alt='Leon Suarez' src='/leonsuarezavataredited_ccexpress.png' />
                   </div>
                   <div className='avatar-info'>
@@ -108,8 +124,8 @@ export const PracticalCases = () => {
                 </div>
             }
 
-            <div className='section-three'>
-              <div className='section-three-fake-image' />
+            <div className='section-seven'>
+              <div className='section-seven-fake-image' />
               <p className='description-paragraph-text blur'>Officia do laboris commodo culpa sunt eiusmod. Ullamco ullamco aliquip commodo do enim ex dolore. Ut velit ad veniam qui adipisicing laborum enim amet. Excepteur ipsum irure cupidatat et excepteur.</p>
             </div>
             {
@@ -138,17 +154,34 @@ export const PracticalCases = () => {
                 </div> :
                 <div className='sticky-banner-wrapper'>
                   <div className='sticky-square-banner'>
-                    <p className='description-paragraph-text-fixed'>BANNER</p>
+                    <p className='description-paragraph-text-fixed'><BsQuestionLg className='banner'/></p>
                   </div>
                 </div>
             }
-
-            <div className='section-seven'></div>
-            <div className='section-eigth'></div>
-            <div className='section-nine'></div>
-            <div className='section-ten'></div>
-            <div className='section-eleven'></div>
-            <div className='section-twelve'></div>
+            {
+              absoluteFeedback ?
+                <div className='section-three'>
+                  <p className='description-paragraph-text blur'>Officia do laboris commodo culpa sunt eiusmod. Ullamco ullamco aliquip commodo do enim ex dolore. Officia do laboris commodo culpa sunt eiusmod. Ullamco ullamco aliquip commodo do enim ex dolore. Officia do laboris commodo culpa sunt eiusmod. Ullamco ullamco aliquip commodo do enim ex dolore. Officia do laboris commodo culpa sunt eiusmod. Ullamco ullamco aliquip commodo do enim ex dolore.</p>
+                </div> :
+                <div className='absolute-section'>
+                  <p className='description-paragraph-text-fixed'>Absolute Position allows us to position any element of the DOM wherever we what without affecting the flow. Like a FeedBack Button.</p>
+                </div>
+            }
+            {
+              absoluteFeedback ?
+                <div className='feedback'>
+                  <div className='feedback-button-inactive'><p className='feedback-button-text-inactive blur'>FeedBack</p></div>
+                </div> :
+                <div className='feedback'>
+                  <div className='feedback-button'><p className='feedback-button-text'>FeedBack</p></div>
+                </div>
+            }
+            <div className='section-nine' />
+            <div className='section-ten'>
+              <p className='description-paragraph-text blur'>Officia do laboris commodo culpa sunt eiusmod. Ullamco ullamco aliquip commodo do enim ex dolore. Officia do laboris commodo culpa sunt eiusmod. Ullamco ullamco aliquip commodo do enim ex dolore. Officia do laboris commodo culpa sunt eiusmod.</p>
+            </div>
+            <div className='section-eleven'/>
+            <div className='footer-section'></div>
           </div>
         </div>
       </DisplayWindow>
